13a6e72d1e7dc8d7f153e8f83741c7233df5409a
Jonathan Dolbee <>
2018-10-26 13:18:44 -0500

    React-Virtualized Fully Implemented!

--- src/Components/VirtualizedSongList.js
+++ src/Components/VirtualizedSongList.js
@@ -25,46 +25,126 @@ class VirtualizedSongList extends Component {
         )
     }*/
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.rowRenderer = this.rowRenderer.bind(this);
     }
 
-    rowRenderer ({
+    users = [
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" },
+        { name: "foo" }
+    ];
+
+    rowRenderer({
         key,         // Unique key within array of rows
         index,       // Index of row within collection
         isScrolling, // The List is currently being scrolled
         isVisible,   // This row is visible within the List (eg it is not an overscanned row)
         style        // Style object to be applied to row (to position it)
-      }) {
+    }) {
         return (
             <div
-            key={key}
-            style={style}
-        >
-            {console.log(index)}
-            {console.log(this.props.songs)}
-            {this.props.songs[index].attributes.name}
-        </div>
+                key={key}
+                style={style}
+            >
+                {this.props.songs[index].attributes.name}
+            </div>
         )
-      }
-
-
+    }
+    /* Great guide on react-virtualized
+     * https://blog.logrocket.com/rendering-large-lists-with-react-virtualized-82741907a6b3
+     */
     render() {
         return (
-            <div>
-                Learn this:
-                https://blog.logrocket.com/rendering-large-lists-with-react-virtualized-82741907a6b3
-
-                <List
-                    width={300}
-                    height={300}
-                    rowCount={this.props.songs.length}
-                    rowHeight={20}
-                    rowRenderer={this.rowRenderer}
-                />
-            </div>
+            <WindowScroller>
+                {({ height, isScrolling, onChildScroll, scrollTop }) => (
+                    <AutoSizer disableHeight>
+                        {({ width }) => (
+                            <List
+                                autoHeight
+                                height={height}
+                                width={width}
+                                scrollTop={scrollTop}
+                                isScrolling={isScrolling}
+                                rowHeight={20}
+                                rowRenderer={this.rowRenderer}
+                                rowCount={this.props.songs.length}
+                                overscanRowCount={5}
+                                style={{ outline: "none" }}
+                            />
+                        )}
+                    </AutoSizer>
+                )}
+            </WindowScroller>
         )
 
 
