8cc10fadcefcbf97853d1b2c5dcd5058861b54bd
LSU\jdolbe1 <jdolbe1@lsu.edu>
2018-11-08 13:21:49 -0600

    Basic playlist view :)

--- src/Components/PlaylistsGridList.js
+++ src/Components/PlaylistsGridList.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Row, Col } from 'antd';
+import { formatImgSrc } from '../consts';
 
 class PlaylistsGridList extends Component {
 
     renderGridItems = () => {
         let items = this.props.playlists.map((playlist) => {
             return (
-                <Col span={6}>
-                    <div style={{backgroundColor: "yellow", height: "400px"}}>
+                <Col key={playlist.id} xs={24} sm={12} md={8} lg={6} xl={6}>
+                    <div style={{ width: "200px", height: "200px", margin: "auto", marginBottom: "30px" }}>
+                        
+                        <img src={formatImgSrc(playlist.attributes.artwork.url, 200, 200)}></img>
                         {playlist.attributes.name}
                     </div>
                 </Col>
@@ -21,7 +24,7 @@ class PlaylistsGridList extends Component {
     render() {
         return (
             <div>
-                <Row gutter={20}>
+                <Row justify="center" gutter={20}>
                     {this.renderGridItems()}
                 </Row>
             </div>
